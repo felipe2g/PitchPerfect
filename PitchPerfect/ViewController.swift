9b184befdb7e332da4f5b34fb5d1503927426469
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-06-07 21:32:30 -0300

    add tap to record label and change text on click to record

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func recordAudio(_ sender: Any) {
-        print("Record button was pressed")
+        recordingLabel.text = "Recording in Progress"
     }
     
 }
